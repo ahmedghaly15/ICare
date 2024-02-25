3e29f8e5a4c89ab62adf8009371a47db51659b8e
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-26 00:20:52 +0200

    Setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:icare/bloc_observer.dart';
+import 'package:icare/firebase_options.dart';
 import 'package:icare/src/icare_app.dart';
 import 'package:icare/service_locator.dart';
 
 //TODO: use flutter_spinkit in forgot password and reset password screens
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   Bloc.observer = MyBlocObserver();
 
