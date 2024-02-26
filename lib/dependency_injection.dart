30fdcd10a2b96bb980146bc9eb6f1a47089ebdfb
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-26 05:38:08 +0200

    Add cloud_firestore package & Setup DI for external, core

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,13 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:icare/src/config/router/app_router.dart';
+import 'package:icare/src/core/network/network_info.dart';
 import 'package:icare/src/features/auth/presentation/cubits/login/login_cubit.dart';
 import 'package:icare/src/features/auth/presentation/cubits/register/register_cubit.dart';
 import 'package:icare/src/features/auth/presentation/cubits/reset_password/reset_password_cubit.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 final GetIt getIt = GetIt.instance;
 
 class DependencyInjection {
-  void setup() {
+  Future<void> setup() async {
+    await _setupForExternal();
+
     _setupForConfig();
 
     _setupForCore();
@@ -41,5 +48,26 @@ class DependencyInjection {
     getIt.registerSingleton<AppRouter>(AppRouter());
   }
 
-  void _setupForCore() {}
+  void _setupForCore() {
+    getIt.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(
+        connectionChecker: getIt.get<InternetConnectionChecker>(),
+      ),
+    );
+  }
+
+  Future<void> _setupForExternal() async {
+    getIt.registerLazySingleton<InternetConnectionChecker>(
+      () => InternetConnectionChecker(),
+    );
+
+    getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+
+    getIt.registerLazySingleton<FirebaseFirestore>(
+      () => FirebaseFirestore.instance,
+    );
+
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+  }
 }
