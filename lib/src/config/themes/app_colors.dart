fb9935c84266d7ff544b9a4ab45eb9105bc10f9e
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-26 04:14:47 +0200

    Add TextButtonTheme to AppThemes

--- lib/src/config/themes/app_colors.dart
+++ lib/src/config/themes/app_colors.dart
@@ -4,5 +4,4 @@ class AppColors {
   static const Color primaryColor = Color(0xffFFB74D);
   static const Color secondaryColor = Color(0xffFFF8ED);
   static const Color scaffoldBackgroundColor = Colors.white;
-  // static const Color fontPrimaryColor = Color(0xff6B4D20);
 }
--- lib/src/config/themes/app_themes.dart
+++ lib/src/config/themes/app_themes.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:icare/src/config/themes/app_colors.dart';
+import 'package:icare/src/config/themes/app_text_styles.dart';
 import 'package:icare/src/core/utils/app_strings.dart';
 
 class AppThemes {
-  static ThemeData get lightTheme => ThemeData(
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
         useMaterial3: true,
         brightness: Brightness.light,
         primaryColor: AppColors.primaryColor,
@@ -12,6 +13,12 @@ class AppThemes {
         appBarTheme: const AppBarTheme(
           backgroundColor: AppColors.scaffoldBackgroundColor,
           elevation: 0,
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            textStyle: AppTextStyles.textStyle13Bold(context),
+            foregroundColor: AppColors.primaryColor,
+          ),
         ),
       );
 }
