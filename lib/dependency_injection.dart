e5825ed4272f41332cf1c7433e289b42dc7ba302
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-26 22:18:53 +0200

    Finish CreateFirestoreUserUseCase & Setup DI for Register feature

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:icare/src/features/auth/domain/usecases/create_firestore_user.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:icare/src/config/router/app_router.dart';
 import 'package:icare/src/core/network/network_info.dart';
 import 'package:icare/src/features/auth/data/datasources/login_datasource.dart';
@@ -12,9 +17,6 @@ import 'package:icare/src/features/auth/domain/usecases/register.dart';
 import 'package:icare/src/features/auth/presentation/cubits/login/login_cubit.dart';
 import 'package:icare/src/features/auth/presentation/cubits/register/register_cubit.dart';
 import 'package:icare/src/features/auth/presentation/cubits/reset_password/reset_password_cubit.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -60,6 +62,10 @@ class DependencyInjection {
 
     getIt.registerLazySingleton<RegisterUseCase>(
       () => RegisterUseCase(getIt.get<RegisterRepo>()),
+    );
+
+    getIt.registerLazySingleton<CreateFirestoreUserUseCase>(
+      () => CreateFirestoreUserUseCase(getIt.get<RegisterRepo>()),
     );
   }
 
