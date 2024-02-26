344951eb3c4c77ec7f0c880d54810e0e2c60e098
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-26 05:40:46 +0200

    Build LoginDataSource & LoginRequestParams model

---
+++ lib/src/features/auth/data/datasources/login_datasource.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:icare/src/features/auth/data/models/login_request_params.dart';
+
+abstract class LoginDataSource {
+  Future<UserCredential> login(LoginRequestParams params); // <>
+}
+
+class LoginDataSourceImpl implements LoginDataSource {
+  @override
+  Future<UserCredential> login(LoginRequestParams params) async {
+    return FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
---
+++ lib/src/features/auth/data/models/login_request_params.dart
@@ -0,0 +1,9 @@
+class LoginRequestParams {
+  final String email;
+  final String password;
+
+  const LoginRequestParams({
+    required this.email,
+    required this.password,
+  });
+}
