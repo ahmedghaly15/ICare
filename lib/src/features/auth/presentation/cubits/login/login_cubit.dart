a744cb7138cbe4c0fccaffaa90d272e93c3b7835
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-26 04:15:59 +0200

    Add LoginForm variables to LoginCubit

--- lib/src/features/auth/presentation/cubits/login/login_cubit.dart
+++ lib/src/features/auth/presentation/cubits/login/login_cubit.dart
@@ -1,9 +1,39 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(const LoginInitial());
+
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+
+  late final FocusNode emailFocusNode;
+  late final FocusNode passwordFocusNode;
+
+  late final GlobalKey<FormState> formKey;
+
+  late AutovalidateMode autovalidateMode;
+
+  void initFormAttributes() {
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    emailFocusNode = FocusNode();
+    passwordFocusNode = FocusNode();
+    formKey = GlobalKey<FormState>();
+    autovalidateMode = AutovalidateMode.disabled;
+  }
+
+  void disposeFocusNodes() {
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
+  }
+
+  void disposeController() {
+    emailController.dispose();
+    passwordController.dispose();
+  }
 
   bool isLoginPassVisible = true;
 
