594702d90f8780c2bfcba09836a10c765df227d2
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-28 11:19:45 +0200

    Finish GetMedical usecase

---
+++ lib/src/features/medical/domain/usecases/get_medical.dart
@@ -0,0 +1,17 @@
+import 'package:icare/src/core/api/api_result.dart';
+import 'package:icare/src/core/entities/no_params.dart';
+import 'package:icare/src/core/usecases/base_usecase.dart';
+import 'package:icare/src/features/medical/data/models/get_medical_response.dart';
+import 'package:icare/src/features/medical/domain/repositories/medical_repo.dart';
+
+class GetMedicalUseCase
+    implements BaseUseCases<ApiResult<GetMedicalResponse>, NoParams> {
+  final MedicalRepo _medicalRepo;
+
+  const GetMedicalUseCase(this._medicalRepo);
+
+  @override
+  Future<ApiResult<GetMedicalResponse>> call(NoParams params) async {
+    return await _medicalRepo.getMedical();
+  }
+}
