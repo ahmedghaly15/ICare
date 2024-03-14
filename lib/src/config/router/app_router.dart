3da4725bb16437d4978df854837df9ab21fa577d
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-03-14 15:11:49 +0200

    Refactor AppRouter file

--- lib/src/config/router/app_router.dart
+++ lib/src/config/router/app_router.dart
@@ -22,6 +22,7 @@ import 'package:icare/src/features/onboarding/presentation/views/onboarding_view
 import 'package:icare/src/features/start/presentation/views/start_view.dart';
 
 part 'app_router.gr.dart';
+part 'app_routes.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'View,Route')
 class AppRouter extends _$AppRouter {
@@ -29,81 +30,7 @@ class AppRouter extends _$AppRouter {
   RouteType get defaultRouteType => const RouteType.material();
 
   @override
-  List<AutoRoute> get routes => [
-        AutoRoute(
-          page: EntryRoute.page,
-          initial: true,
-        ),
-        AutoRoute(page: OnboardingRoute.page),
-        AutoRoute(page: StartRoute.page),
-        AutoRoute(
-          page: AuthRoute.page,
-          children: <AutoRoute>[
-            _buildCustomRoute(
-              initial: true,
-              page: LoginRoute.page,
-            ),
-            _buildCustomRoute(page: RegisterRoute.page),
-            _buildCustomRoute(page: ForgotPasswordRoute.page),
-          ],
-        ),
-        AutoRoute(
-          page: BottomNavBarRoute.page,
-          children: <AutoRoute>[
-            AutoRoute(
-              page: LayoutRoute.page,
-              initial: true,
-              children: <AutoRoute>[
-                AutoRoute(
-                  page: HomeRoute.page,
-                  initial: true,
-                ),
-                AutoRoute(
-                  page: MedicalRoute.page,
-                ),
-                AutoRoute(page: BabyRoute.page),
-              ],
-            ),
-          ],
-        ),
-        _buildCustomRoute(
-          page: EmergencyDiseasesRoute.page,
-          transitionsBuilder: TransitionsBuilders.fadeIn,
-        ),
-        _buildCustomRoute(
-          page: MedicalInfoRoute.page,
-          transitionsBuilder: TransitionsBuilders.fadeIn,
-        ),
-        _buildCustomRoute(
-          page: MedicalInfoCategoryDiseasesRoute.page,
-          transitionsBuilder: TransitionsBuilders.fadeIn,
-        ),
-        _buildCustomRoute(
-          page: EmergencyDiseaseDetailsRoute.page,
-          transitionsBuilder: TransitionsBuilders.slideBottom,
-        ),
-        _buildCustomRoute(
-          page: MedicalInfoDiseaseDetailsRoute.page,
-          transitionsBuilder: TransitionsBuilders.slideBottom,
-        ),
-        _buildCustomRoute(page: BabyCryPredictorRoute.page),
-      ];
-
-  CustomRoute _buildCustomRoute({
-    bool initial = false,
-    required PageInfo<dynamic> page,
-    List<AutoRoute>? children,
-    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
-        transitionsBuilder,
-  }) {
-    return CustomRoute(
-      initial: initial,
-      page: page,
-      transitionsBuilder: transitionsBuilder ?? AppConstants.transitionsBuilder,
-      durationInMilliseconds: AppConstants.transitionDuration,
-      children: children,
-    );
-  }
+  List<AutoRoute> get routes => appRoutes;
 }
 
 @RoutePage(name: RoutesNames.authRoute)
---
+++ lib/src/config/router/app_routes.dart
@@ -0,0 +1,79 @@
+part of 'app_router.dart';
+
+List<AutoRoute> get appRoutes {
+  return <AutoRoute>[
+    AutoRoute(
+      page: EntryRoute.page,
+      initial: true,
+    ),
+    AutoRoute(page: OnboardingRoute.page),
+    AutoRoute(page: StartRoute.page),
+    AutoRoute(
+      page: AuthRoute.page,
+      children: <AutoRoute>[
+        _buildCustomRoute(
+          initial: true,
+          page: LoginRoute.page,
+        ),
+        _buildCustomRoute(page: RegisterRoute.page),
+        _buildCustomRoute(page: ForgotPasswordRoute.page),
+      ],
+    ),
+    AutoRoute(
+      page: BottomNavBarRoute.page,
+      children: <AutoRoute>[
+        AutoRoute(
+          page: LayoutRoute.page,
+          initial: true,
+          children: <AutoRoute>[
+            AutoRoute(
+              page: HomeRoute.page,
+              initial: true,
+            ),
+            AutoRoute(
+              page: MedicalRoute.page,
+            ),
+            AutoRoute(page: BabyRoute.page),
+          ],
+        ),
+      ],
+    ),
+    _buildCustomRoute(
+      page: EmergencyDiseasesRoute.page,
+      transitionsBuilder: TransitionsBuilders.fadeIn,
+    ),
+    _buildCustomRoute(
+      page: MedicalInfoRoute.page,
+      transitionsBuilder: TransitionsBuilders.fadeIn,
+    ),
+    _buildCustomRoute(
+      page: MedicalInfoCategoryDiseasesRoute.page,
+      transitionsBuilder: TransitionsBuilders.fadeIn,
+    ),
+    _buildCustomRoute(
+      page: EmergencyDiseaseDetailsRoute.page,
+      transitionsBuilder: TransitionsBuilders.slideBottom,
+    ),
+    _buildCustomRoute(
+      page: MedicalInfoDiseaseDetailsRoute.page,
+      transitionsBuilder: TransitionsBuilders.slideBottom,
+    ),
+    _buildCustomRoute(page: BabyCryPredictorRoute.page),
+  ];
+}
+
+CustomRoute _buildCustomRoute({
+  bool initial = false,
+  required PageInfo<dynamic> page,
+  List<AutoRoute>? children,
+  Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+      transitionsBuilder,
+}) {
+  return CustomRoute(
+    initial: initial,
+    page: page,
+    transitionsBuilder: transitionsBuilder ?? AppConstants.transitionsBuilder,
+    durationInMilliseconds: AppConstants.transitionDuration,
+    children: children,
+  );
+}
