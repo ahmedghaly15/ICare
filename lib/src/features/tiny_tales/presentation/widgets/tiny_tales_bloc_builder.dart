19b5d39d935033f325b61c0554f70ff8d3d50dd4
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-03-29 16:31:17 +0200

    Build TinyTalesBlocBuilder widget

---
+++ lib/src/features/tiny_tales/presentation/widgets/tiny_tales_bloc_builder.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:icare/src/core/widgets/custom_circular_progress_indicator.dart';
+import 'package:icare/src/core/widgets/custom_error_widget.dart';
+import 'package:icare/src/features/tiny_tales/presentation/cubits/tiny_tales_cubit.dart';
+import 'package:icare/src/features/tiny_tales/presentation/cubits/tiny_tales_state.dart';
+import 'package:icare/src/features/tiny_tales/presentation/widgets/tiny_tale_item.dart';
+
+class TinyTalesBlocBuilder extends StatelessWidget {
+  const TinyTalesBlocBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TinyTalesCubit, TinyTalesState>(
+      buildWhen: (_, current) =>
+          current is GetTinyTalesSuccess ||
+          current is GetTinyTalesLoading ||
+          current is GetTinyTalesError,
+      builder: (context, state) {
+        if (state is GetTinyTalesSuccess) {
+          return SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => const TinyTaleItem(),
+              childCount: 10,
+            ),
+          );
+        } else if (state is GetTinyTalesError) {
+          return SliverFillRemaining(
+            hasScrollBody: false,
+            child: CustomErrorWidget(error: state.error),
+          );
+        } else {
+          return const SliverFillRemaining(
+            child: Center(
+              child: CustomCircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
