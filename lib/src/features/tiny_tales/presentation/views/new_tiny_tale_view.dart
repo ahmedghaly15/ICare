09385d906471800cddcced630aff51c21c892dea
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-03-30 17:47:14 +0200

    Refactor NewTinyTaleView  - Update & Refactor NewTinyTaleViewBody

--- lib/src/features/tiny_tales/presentation/views/new_tiny_tale_view.dart
+++ lib/src/features/tiny_tales/presentation/views/new_tiny_tale_view.dart
@@ -2,7 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:icare/dependency_injection.dart';
-import 'package:icare/src/features/tiny_tales/presentation/cubits/tiny_tales_cubit.dart';
+import 'package:icare/src/features/tiny_tales/presentation/cubits/new_tiny_tale/new_tiny_tale_cubit.dart';
 import 'package:icare/src/features/tiny_tales/presentation/widgets/new_tiny_tale_view_body.dart';
 
 @RoutePage()
@@ -11,8 +11,8 @@ class NewTinyTaleView extends StatelessWidget implements AutoRouteWrapper {
 
   @override
   Widget wrappedRoute(BuildContext context) {
-    return BlocProvider<TinyTalesCubit>(
-      create: (_) => getIt.get<TinyTalesCubit>()..getTinyTales(),
+    return BlocProvider<NewTinyTaleCubit>(
+      create: (_) => getIt.get<NewTinyTaleCubit>(),
       child: this,
     );
   }
--- lib/src/features/tiny_tales/presentation/widgets/new_tiny_tale_view_body.dart
+++ lib/src/features/tiny_tales/presentation/widgets/new_tiny_tale_view_body.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:icare/src/core/helpers/helper.dart';
-import 'package:icare/src/core/widgets/custom_text_form_field.dart';
 import 'package:icare/src/core/widgets/my_sized_box.dart';
-import 'package:icare/src/features/tiny_tales/presentation/cubits/tiny_tales_cubit.dart';
 import 'package:icare/src/features/tiny_tales/presentation/widgets/add_photo_text_button.dart';
-import 'package:icare/src/features/tiny_tales/presentation/widgets/new_tiny_tale_image.dart';
+import 'package:icare/src/features/tiny_tales/presentation/widgets/linear_progress_indicator_bloc_builder.dart';
+import 'package:icare/src/features/tiny_tales/presentation/widgets/new_tiny_tale_image_bloc_builder.dart';
 import 'package:icare/src/features/tiny_tales/presentation/widgets/new_tiny_tale_sliver_app_bar.dart';
-import 'package:icare/src/features/tiny_tales/presentation/widgets/user_name_and_image.dart';
+import 'package:icare/src/features/tiny_tales/presentation/widgets/new_tiny_tale_text_field_bloc_builder.dart';
+import 'package:icare/src/features/tiny_tales/presentation/widgets/current_user_name_and_image.dart';
 
 class NewTinyTaleViewBody extends StatelessWidget {
   const NewTinyTaleViewBody({super.key});
@@ -16,63 +14,42 @@ class NewTinyTaleViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: CustomScrollView(
-        slivers: [
-          SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: 9.w),
-            sliver: const NewTinyTaleSliverAppBar(),
-          ),
-          SliverPadding(
-            padding: EdgeInsets.only(
-              top: 32.h,
-              right: 16.w,
-              left: 16.w,
-            ),
-            sliver: SliverToBoxAdapter(
-              child: Column(
-                children: <Widget>[
-                  const UsernameAndImage(),
-                  CustomTextFormField(
-                    maxLines: null,
-                    controller: context
-                        .read<TinyTalesCubit>()
-                        .createNewTinyTaleController,
-                    hintText:
-                        "What's in your mind, ${Helper.currentUser!.name!.split(' ')[0]}",
-                    enableSuggestions: true,
-                    contentPadding: EdgeInsets.zero,
-                    filled: false,
-                    textCapitalization: TextCapitalization.sentences,
-                    keyboardType: TextInputType.multiline,
-                    border: InputBorder.none,
-                    onChanged: (String value) => context
-                        .read<TinyTalesCubit>()
-                        .setNewTinyTaleText(value),
-                  ),
-                  MySizedBox.height10,
-                  // const NewTinyTaleImage(),
-                ],
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 9.w),
+        child: CustomScrollView(
+          slivers: [
+            const NewTinyTaleSliverAppBar(),
+            SliverPadding(
+              padding: EdgeInsets.only(top: 24.h),
+              sliver: const SliverToBoxAdapter(
+                child: Column(
+                  children: <Widget>[
+                    LinearProgressIndicatorBlocBuilder(),
+                    CurrentUsernameAndImage(),
+                    NewTinyTaleTextFieldBlocBuilder(),
+                    MySizedBox.height10,
+                    NewTinyTaleImageBlocBuilder(),
+                  ],
+                ),
               ),
             ),
-          ),
-          SliverPadding(
-            padding: EdgeInsets.only(
-              bottom: 32.h,
-              right: 16.w,
-              left: 16.w,
-              top: 12.h,
-            ),
-            sliver: const SliverFillRemaining(
-              hasScrollBody: false,
-              child: Column(
-                children: <Widget>[
-                  Spacer(),
-                  AddPhotoTextButton(),
-                ],
+            SliverPadding(
+              padding: EdgeInsets.only(
+                bottom: 32.h,
+                top: 12.h,
+              ),
+              sliver: const SliverFillRemaining(
+                hasScrollBody: false,
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    AddPhotoTextButton(),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
