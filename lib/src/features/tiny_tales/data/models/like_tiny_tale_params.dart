6f0f80ff8cb35881362f5b4ddcec64a67dbc5d0c
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-03-30 17:35:36 +0200

    Update LikeTinyTale usecase & Delete some files

---
+++ lib/src/features/tiny_tales/data/models/like_tiny_tale_params.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LikeTinyTaleParams {
+  final String tinyTaleId;
+  final BuildContext context;
+
+  const LikeTinyTaleParams({
+    required this.tinyTaleId,
+    required this.context,
+  });
+}
--- lib/src/features/tiny_tales/domain/usecases/like_tiny_tale.dart
+++ lib/src/features/tiny_tales/domain/usecases/like_tiny_tale.dart
@@ -1,14 +1,15 @@
 import 'package:icare/src/core/firebase/firebase_request_result.dart';
 import 'package:icare/src/core/usecases/firebase_usecase.dart';
+import 'package:icare/src/features/tiny_tales/data/models/like_tiny_tale_params.dart';
 import 'package:icare/src/features/tiny_tales/domain/repositories/tiny_tales_repo.dart';
 
-class LikeTinyTaleUseCase implements FirebaseUseCase<void, String> {
+class LikeTinyTaleUseCase implements FirebaseUseCase<void, LikeTinyTaleParams> {
   final TinyTalesRepo _tinyTalesRepo;
 
   const LikeTinyTaleUseCase(this._tinyTalesRepo);
 
   @override
-  Future<FirebaseRequestResult<void>> call(String params) async {
+  Future<FirebaseRequestResult<void>> call(LikeTinyTaleParams params) async {
     return await _tinyTalesRepo.likeTinyTale(params);
   }
 }
