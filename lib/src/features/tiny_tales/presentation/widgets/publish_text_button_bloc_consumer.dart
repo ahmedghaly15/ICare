4a93cff51be231a6ca2d951b513de2e604900854
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-03-30 17:39:49 +0200

    Build PublishTextButtonBlocConsumer widget

---
+++ lib/src/features/tiny_tales/presentation/widgets/publish_text_button_bloc_consumer.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:icare/src/config/themes/app_colors.dart';
+import 'package:icare/src/config/themes/app_text_styles.dart';
+import 'package:icare/src/core/utils/app_strings.dart';
+import 'package:icare/src/features/tiny_tales/presentation/cubits/new_tiny_tale/new_tiny_tale_cubit.dart';
+import 'package:icare/src/features/tiny_tales/presentation/cubits/new_tiny_tale/new_tiny_tale_state.dart';
+
+class PublishTextButtonBlocConsumer extends StatelessWidget {
+  const PublishTextButtonBlocConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<NewTinyTaleCubit, NewTinyTaleState>(
+      listenWhen: (_, current) =>
+          current is CreateTinyTaleSuccess ||
+          current is CreateTinyTaleError ||
+          current is CreateTinyTaleLoading,
+      listener: (context, state) {
+        context
+            .read<NewTinyTaleCubit>()
+            .handleCreateNewTinyTaleStates(state, context);
+      },
+      buildWhen: (_, current) =>
+          current is SetNewTextValue ||
+          current is PickTinyTaleImageSuccess ||
+          current is RemoveTinyTaleImage,
+      builder: (context, state) {
+        return TextButton(
+          onPressed:
+              context.read<NewTinyTaleCubit>().publishNewTinyTale(context),
+          style: ButtonStyle(
+            padding: MaterialStatePropertyAll(EdgeInsets.all(8.h)),
+            shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(24.r)),
+              ),
+            ),
+            backgroundColor: MaterialStateProperty.resolveWith<Color>(
+              (Set<MaterialState> states) {
+                if (states.contains(MaterialState.disabled)) {
+                  return AppColors.lightGrey;
+                }
+                return AppColors.primaryColor;
+              },
+            ),
+            foregroundColor: const MaterialStatePropertyAll(Colors.white),
+            textStyle: MaterialStateProperty.resolveWith<TextStyle>(
+              (Set<MaterialState> states) {
+                if (states.contains(MaterialState.disabled)) {
+                  return AppTextStyles.textStyle20Regular(context);
+                }
+                return AppTextStyles.textStyle20Bold(context);
+              },
+            ),
+          ),
+          child: const Text(AppStrings.publish),
+        );
+      },
+    );
+  }
+}
