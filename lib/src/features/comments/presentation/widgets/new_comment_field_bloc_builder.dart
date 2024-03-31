95548778b189e41eff7557086f442beae2c2cc05
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-03-31 16:58:59 +0200

    Build NewCommentFieldBlocBuilder & TypeCommentFieldAndButtons widgets

---
+++ lib/src/features/comments/presentation/widgets/new_comment_field_bloc_builder.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:icare/src/core/utils/app_strings.dart';
+import 'package:icare/src/core/widgets/my_sized_box.dart';
+import 'package:icare/src/core/widgets/send_message_text_field.dart';
+import 'package:icare/src/features/comments/presentation/cubits/comments_cubit.dart';
+import 'package:icare/src/features/comments/presentation/cubits/comments_state.dart';
+import 'package:icare/src/features/comments/presentation/widgets/picked_comment_image.dart';
+
+class NewCommentFieldBlocBuilder extends StatelessWidget {
+  const NewCommentFieldBlocBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CommentsCubit, CommentsState>(
+      buildWhen: (_, current) =>
+          current is PickCommentImageSuccess ||
+          current is RemovePickedCommentImage,
+      builder: (context, state) {
+        return Expanded(
+          child: Column(
+            children: <Widget>[
+              SendMessageTextField(
+                hintText: AppStrings.writeAComment,
+                controller: context.read<CommentsCubit>().commentController,
+                onChanged: (String value) =>
+                    context.read<CommentsCubit>().setNewTextValue(value),
+              ),
+              if (context.read<CommentsCubit>().commentImage != null) ...[
+                MySizedBox.height8,
+                const PickedCommentImage(),
+              ],
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/src/features/comments/presentation/widgets/type_comment_field_and_buttons.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:icare/src/core/widgets/pick_image_icon_button.dart';
+import 'package:icare/src/features/comments/presentation/cubits/comments_cubit.dart';
+import 'package:icare/src/features/comments/presentation/widgets/new_comment_field_bloc_builder.dart';
+import 'package:icare/src/features/comments/presentation/widgets/type_new_comment_button_bloc_consumer.dart';
+import 'package:image_picker/image_picker.dart';
+
+class TypeCommentFieldAndButtons extends StatelessWidget {
+  const TypeCommentFieldAndButtons({
+    super.key,
+    required this.tinyTaleId,
+  });
+
+  final String tinyTaleId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        PickImageIconButton(
+          icon: Icons.photo_camera,
+          onPressed: () => context
+              .read<CommentsCubit>()
+              .pickCommentImage(ImageSource.camera),
+        ),
+        PickImageIconButton(
+          icon: Icons.image,
+          onPressed: () => context
+              .read<CommentsCubit>()
+              .pickCommentImage(ImageSource.gallery),
+        ),
+        const NewCommentFieldBlocBuilder(),
+        SizedBox.square(dimension: 8.h),
+        TypeNewCommentButtonBlocConsumer(
+          tinyTaleId: tinyTaleId,
+        ),
+      ],
+    );
+  }
+}
