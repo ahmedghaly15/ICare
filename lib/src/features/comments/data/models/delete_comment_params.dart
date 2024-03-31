9425378b0faffaaf3445ca9e5eaf19da4d3fe637
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-03-31 21:02:32 +0200

    Update TypeNewCommentParams & DeleteCommentParams models

--- lib/src/features/comments/data/models/delete_comment_params.dart
+++ lib/src/features/comments/data/models/delete_comment_params.dart
@@ -1,6 +1,11 @@
 class DeleteCommentParams {
   final String? tinyTaleId;
   final String? commentId;
+  final String? commentReplyId;
 
-  const DeleteCommentParams({this.tinyTaleId, this.commentId});
+  const DeleteCommentParams({
+    this.tinyTaleId,
+    this.commentId,
+    this.commentReplyId,
+  });
 }
--- lib/src/features/comments/data/models/type_new_comment_params.dart
+++ lib/src/features/comments/data/models/type_new_comment_params.dart
@@ -4,11 +4,13 @@ import 'package:icare/src/features/comments/data/models/comment_data.dart';
 class TypeNewCommentParams {
   final CommentData? commentData;
   final String? tinyTaleId;
+  final String? commentId;
   final BuildContext context;
 
   const TypeNewCommentParams({
     this.tinyTaleId,
     this.commentData,
+    this.commentId,
     required this.context,
   });
 }
