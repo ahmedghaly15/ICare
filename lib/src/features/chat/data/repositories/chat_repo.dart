9cb05b3b6b5c6ff0a962ce6dbfbe7da54efb4348
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-03 14:08:23 +0200

    Finish ChatRepo

---
+++ lib/src/features/chat/data/repositories/chat_repo.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:icare/src/core/firebase/firebase_request_result.dart';
+import 'package:icare/src/core/utils/functions/execute_and_handle_firebase_errors.dart';
+import 'package:icare/src/features/chat/data/datasources/chat_datasource.dart';
+import 'package:icare/src/features/chat/data/models/message_model.dart';
+import 'package:icare/src/features/chat/data/models/send_message_params.dart';
+
+class ChatRepo {
+  final ChatDatasource _chatDatasource;
+
+  const ChatRepo(this._chatDatasource);
+
+  Future<FirebaseRequestResult<void>> sendMessage(
+    SendMessageParams params,
+  ) async {
+    return executeAndHandleFirebaseErrors<void>(
+      () async => await _chatDatasource.sendMessage(params),
+    );
+  }
+
+  Future<FirebaseRequestResult<List<MessageModel>>> streamMessages(
+    String receiverId,
+  ) {
+    return executeAndHandleFirebaseErrors<List<MessageModel>>(
+      () async {
+        List<MessageModel> messages = <MessageModel>[];
+        _chatDatasource.streamMessages(receiverId).listen((snapshot) {
+          snapshot.docs.map(
+            (doc) => messages.add(MessageModel.fromJson(doc.data())),
+          );
+        });
+        return messages;
+      },
+    );
+  }
+
+  Future<FirebaseRequestResult<TaskSnapshot>> uploadMessageImage(
+    File? messageImage,
+  ) {
+    return executeAndHandleFirebaseErrors<TaskSnapshot>(
+      () async => await _chatDatasource.uploadMessageImage(messageImage),
+    );
+  }
+}
