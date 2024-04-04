195d98917d440a0597f8e47eb4239ef3f2f7bd14
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-04 16:50:05 +0200

    Finish search feature UI  - Finish SearchUsersView  - Build ShimmerSearchUser widget  - Build ShimmerSearchUsers widget  - Build ICareCommunityDialog widget

---
+++ lib/src/features/search/presentation/widgets/shimmer_search_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:icare/src/features/search/presentation/widgets/shimmer_search_user.dart';
+
+class ShimmerSearchUsers extends StatelessWidget {
+  const ShimmerSearchUsers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(vertical: 16.w),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return const ShimmerSearchUser();
+          },
+          childCount: 8,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/features/tiny_tales/presentation/widgets/icare_community_dialog.dart
@@ -0,0 +1,66 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:icare/src/config/router/app_router.dart';
+import 'package:icare/src/config/themes/app_text_styles.dart';
+import 'package:icare/src/core/utils/app_constants.dart';
+import 'package:icare/src/core/utils/app_strings.dart';
+import 'package:icare/src/core/widgets/custom_animated_dialog.dart';
+import 'package:icare/src/core/widgets/my_sized_box.dart';
+import 'package:icare/src/features/tiny_tales/presentation/cubits/tiny_tales/tiny_tales_cubit.dart';
+
+class ICareCommunityDialog {
+  static Future show(BuildContext context) async {
+    return await showGeneralDialog(
+      context: context,
+      barrierDismissible: true,
+      barrierLabel: '',
+      pageBuilder: (context, _, __) => const SizedBox.shrink(),
+      transitionDuration: AppConstants.dialogsTransitionDuration,
+      transitionBuilder: (context, animation1, animation2, widget) {
+        return CustomAnimatedDialog(
+          animation1: animation1,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              MySizedBox.height8,
+              ListTile(
+                title: TextButton.icon(
+                  onPressed: () {
+                    context.maybePop().then((value) {
+                      context.pushRoute(const SearchUsersRoute());
+                    });
+                  },
+                  icon: const Icon(Icons.search),
+                  label: const Text(AppStrings.search),
+                  style: TextButton.styleFrom(
+                    textStyle: AppTextStyles.textStyle18Bold(context),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: TextButton.icon(
+                  onPressed: () {
+                    context.maybePop().then((value) {
+                      context
+                          .read<TinyTalesCubit>()
+                          .getBookmarkedTinyTales()
+                          .then((value) {
+                        context.pushRoute(const BookmarkedTinyTalesRoute());
+                      });
+                    });
+                  },
+                  icon: const Icon(Icons.bookmark),
+                  label: const Text(AppStrings.bookmarks),
+                  style: TextButton.styleFrom(
+                    textStyle: AppTextStyles.textStyle18Bold(context),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
