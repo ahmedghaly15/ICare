2d62ab53be6f1f2e2a31304de060a5f2dce18b50
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-04 16:48:46 +0200

    Update ICareCommunitySliverAppBar widget

--- lib/src/features/tiny_tales/presentation/widgets/icare_community_sliver_app_bar.dart
+++ lib/src/features/tiny_tales/presentation/widgets/icare_community_sliver_app_bar.dart
@@ -1,12 +1,8 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:icare/src/config/router/app_router.dart';
 import 'package:icare/src/core/utils/app_assets.dart';
 import 'package:icare/src/core/utils/app_strings.dart';
 import 'package:icare/src/core/widgets/custom_sliver_app_bar.dart';
-import 'package:icare/src/features/tiny_tales/presentation/cubits/tiny_tales/tiny_tales_cubit.dart';
+import 'package:icare/src/features/tiny_tales/presentation/widgets/icare_community_dialog.dart';
 
 class ICareCommunitySliverAppBar extends StatelessWidget {
   const ICareCommunitySliverAppBar({super.key});
@@ -16,18 +12,11 @@ class ICareCommunitySliverAppBar extends StatelessWidget {
     return CustomSliverAppBar(
       title: AppStrings.icareCommunity,
       actions: <Widget>[
-        Padding(
-          padding: EdgeInsets.all(8.0.h),
-          child: GestureDetector(
-              onTap: () {
-                context
-                    .read<TinyTalesCubit>()
-                    .getBookmarkedTinyTales()
-                    .then((value) {
-                  context.pushRoute(const BookmarkedTinyTalesRoute());
-                });
-              },
-              child: Image.asset(AppAssets.imagesAppLogo)),
+        IconButton(
+          onPressed: () {
+            ICareCommunityDialog.show(context);
+          },
+          icon: Image.asset(AppAssets.imagesAppLogo),
         ),
       ],
     );
