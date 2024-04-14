f78e53abfc6c6663accc5f462852854213d18639
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-14 08:08:49 +0200

    Handle cases in UpdateUser in EditProfileDatasource

--- lib/src/features/profile/data/datasources/edit_profile_datasource.dart
+++ lib/src/features/profile/data/datasources/edit_profile_datasource.dart
@@ -26,17 +26,22 @@ class EditProfileDatasourceImpl implements EditProfileDatasource {
       password: params.password ?? Helper.currentUser!.password,
     );
     Helper.currentUser = user;
-    await getIt
-        .get<FirebaseFirestore>()
-        .collection(AppStrings.usersCollection)
-        .doc(Helper.uId)
-        .update(user.toJson());
+    await _accessUsersCollection().doc(Helper.uId).update(user.toJson());
     await _updateUserTinyTales(user);
     await _updateUserTinyTalesLikes(user);
     await _updateUserComments(user);
     await _updateUserCommentsLikes(user);
     await _updateUserCommentsReplies(user);
     await _updateUserCommentsRepliesLikes(user);
+    await _updateUserBookmarkedTinyTales(user);
+    await _updateUserInOtherUsersFollowing(user);
+    await _updateUserInOtherUsersFollowers(user);
+  }
+
+  CollectionReference<Map<String, dynamic>> _accessUsersCollection() {
+    return getIt
+        .get<FirebaseFirestore>()
+        .collection(AppStrings.usersCollection);
   }
 
   @override
@@ -51,7 +56,6 @@ class EditProfileDatasourceImpl implements EditProfileDatasource {
 
   Future<void> _updateUserTinyTales(ICareUser user) async {
     final tinyTalesQuery = await _accessTinyTalesCollection().get();
-
     for (final tinyTale in tinyTalesQuery.docs) {
       if (tinyTale.data()['user']?['uId'] == Helper.uId) {
         await _accessTinyTalesCollection().doc(tinyTale.id).update(
@@ -68,7 +72,6 @@ class EditProfileDatasourceImpl implements EditProfileDatasource {
     for (final tinyTale in tinyTalesQuery.docs) {
       final tinyTaleLikes =
           await _accessTinyTaleLikesCollection(tinyTale.id).get();
-
       for (final like in tinyTaleLikes.docs) {
         if (like.id == Helper.uId) {
           await _accessTinyTaleLikesCollection(tinyTale.id)
@@ -87,7 +90,6 @@ class EditProfileDatasourceImpl implements EditProfileDatasource {
     final tinyTalesQuery = await _accessTinyTalesCollection().get();
     for (final tinyTale in tinyTalesQuery.docs) {
       final comments = await _accessCommentsCollection(tinyTale.id).get();
-
       for (final comment in comments.docs) {
         if (comment.data()['user']?['uId'] == Helper.uId) {
           await _accessCommentsCollection(tinyTale.id).doc(comment.id).update(
@@ -107,7 +109,6 @@ class EditProfileDatasourceImpl implements EditProfileDatasource {
       for (final comment in comments.docs) {
         final commentLikes =
             await _accessCommentLikesCollection(tinyTale.id, comment.id).get();
-
         for (final like in commentLikes.docs) {
           if (like.id == Helper.uId) {
             await _accessCommentLikesCollection(tinyTale.id, comment.id)
@@ -154,7 +155,6 @@ class EditProfileDatasourceImpl implements EditProfileDatasource {
         final commentReplies =
             await _accessCommentRepliesCollection(tinyTale.id, comment.id)
                 .get();
-
         for (final reply in commentReplies.docs) {
           final commentRepliesLikes =
               await _accessCommentRepliesLikesCollection(
@@ -175,6 +175,70 @@ class EditProfileDatasourceImpl implements EditProfileDatasource {
         }
       }
     }
+  }
+
+  Future<void> _updateUserBookmarkedTinyTales(ICareUser user) async {
+    final bookmarkedTinyTalesQuery =
+        await _accessBookmarkedTinyTalesCollection().get();
+    for (final tinyTale in bookmarkedTinyTalesQuery.docs) {
+      if (tinyTale.data()['user']?['uId'] == Helper.uId) {
+        await _accessBookmarkedTinyTalesCollection().doc(tinyTale.id).update(
+          {
+            'user': user.toJson(),
+          },
+        );
+      }
+    }
+  }
+
+  Future<void> _updateUserInOtherUsersFollowing(ICareUser user) async {
+    final usersQuery = await _accessUsersCollection().get();
+    for (final queryUser in usersQuery.docs) {
+      final followingQuery =
+          await _accessUserFollowingCollection(queryUser).get();
+      for (final following in followingQuery.docs) {
+        if (following.id == Helper.uId) {
+          await _accessUserFollowingCollection(queryUser)
+              .doc(Helper.uId)
+              .update(user.toJson());
+        }
+      }
+    }
+  }
+
+  Future<void> _updateUserInOtherUsersFollowers(ICareUser user) async {
+    final usersQuery = await _accessUsersCollection().get();
+    for (final queryUser in usersQuery.docs) {
+      final followersQuery =
+          await _accessUserFollowersCollection(queryUser).get();
+      for (final follower in followersQuery.docs) {
+        if (follower.id == Helper.uId) {
+          await _accessUserFollowersCollection(queryUser)
+              .doc(Helper.uId)
+              .update(user.toJson());
+        }
+      }
+    }
+  }
+
+  CollectionReference<Map<String, dynamic>> _accessUserFollowersCollection(
+      QueryDocumentSnapshot<Map<String, dynamic>> user) {
+    return _accessUsersCollection()
+        .doc(user.id)
+        .collection(AppStrings.followersCollection);
+  }
+
+  CollectionReference<Map<String, dynamic>> _accessUserFollowingCollection(
+          QueryDocumentSnapshot<Map<String, dynamic>> user) =>
+      _accessUsersCollection()
+          .doc(user.id)
+          .collection(AppStrings.followingCollection);
+
+  CollectionReference<Map<String, dynamic>>
+      _accessBookmarkedTinyTalesCollection() {
+    return _accessUsersCollection()
+        .doc(Helper.uId)
+        .collection(AppStrings.bookmarkedTinyTalesCollection);
   }
 
   CollectionReference<Map<String, dynamic>> _accessCommentLikesCollection(
