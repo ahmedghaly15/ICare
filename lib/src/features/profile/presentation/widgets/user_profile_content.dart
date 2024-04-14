b61159268e7df4b6eca23673bdfafef01a4f287e
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-14 10:31:50 +0200

    Finish UserProfileView

---
+++ lib/src/features/profile/presentation/widgets/user_profile_content.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:icare/src/config/themes/app_text_styles.dart';
+import 'package:icare/src/core/models/icare_user.dart';
+import 'package:icare/src/core/widgets/followers_and_following_row.dart';
+import 'package:icare/src/core/widgets/my_sized_box.dart';
+import 'package:icare/src/core/widgets/primary_button.dart';
+import 'package:icare/src/features/profile/presentation/widgets/custom_profile_tab_bar.dart';
+
+class UserProfileContent extends StatelessWidget {
+  const UserProfileContent({super.key, required this.user});
+
+  final ICareUser user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: FollowersAndFollowingRow(user: user),
+        ),
+        MySizedBox.height10,
+        Text(
+          user.name!,
+          style: AppTextStyles.textStyle25Bold(context),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+        MySizedBox.height5,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Icon(
+              Icons.email,
+              color: Colors.black,
+            ),
+            MySizedBox.width10,
+            Text(
+              user.email!,
+              style: AppTextStyles.textStyle16Regular(context),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+        MySizedBox.height15,
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: PrimaryButton(
+                  height: 32,
+                  onPressed: () {},
+                  text: 'Follow',
+                ),
+              ),
+              Expanded(
+                child: PrimaryButton(
+                  height: 32,
+                  onPressed: () {},
+                  text: 'Message',
+                ),
+              ),
+            ],
+          ),
+        ),
+        MySizedBox.height27,
+        Expanded(child: CustomProfileTabBar(uId: user.uId!)),
+      ],
+    );
+  }
+}
---
+++ lib/src/features/profile/presentation/widgets/user_profile_view_body.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:icare/src/core/models/icare_user.dart';
+import 'package:icare/src/core/widgets/custom_cached_network_image.dart';
+import 'package:icare/src/features/profile/presentation/widgets/user_profile_content.dart';
+
+class UserProfileViewBody extends StatelessWidget {
+  const UserProfileViewBody({super.key, required this.user});
+
+  final ICareUser user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 5.w,
+            vertical: 16.h,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25.r),
+              topLeft: Radius.circular(25.r),
+            ),
+          ),
+          child: UserProfileContent(user: user),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          top: -65.h,
+          child: CustomCachedNetworkImage(
+            imageUrl: user.profileImage!,
+            imageBuilder: (_, image) => CircleAvatar(
+              radius: 60.r,
+              backgroundImage: image,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
