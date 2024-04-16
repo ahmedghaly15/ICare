a669c8e78efa7d8589caf3762161519db535c737
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-16 06:33:39 +0200

    Refactor ChatDetailsView & Build MessagesStreamBuilder

--- lib/src/features/chat/presentation/views/chat_details_view.dart
+++ lib/src/features/chat/presentation/views/chat_details_view.dart
@@ -1,29 +1,17 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:icare/dependency_injection.dart';
 import 'package:icare/src/core/models/icare_user.dart';
 import 'package:icare/src/core/utils/app_constants.dart';
-import 'package:icare/src/features/chat/presentation/cubit/chat_cubit.dart';
 import 'package:icare/src/features/chat/presentation/widgets/chat_details_custom_app_bar.dart';
 import 'package:icare/src/features/chat/presentation/widgets/message_field_and_buttons.dart';
 import 'package:icare/src/features/chat/presentation/widgets/messages_bloc_builder.dart';
 
 @RoutePage()
-class ChatDetailsView extends StatelessWidget implements AutoRouteWrapper {
+class ChatDetailsView extends StatelessWidget {
   const ChatDetailsView({super.key, required this.receiver});
 
   final ICareUser receiver;
-
-  @override
-  Widget wrappedRoute(BuildContext context) {
-    return BlocProvider<ChatCubit>(
-      create: (_) => getIt.get<ChatCubit>()..streamMessages(receiver.uId!),
-      child: this,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +25,7 @@ class ChatDetailsView extends StatelessWidget implements AutoRouteWrapper {
                 color: Colors.black.withOpacity(0.15),
               ),
             ),
-            MessagesBlocBuilder(receiverId: receiver.uId!),
+            MessagesStreamBuilder(receiverId: receiver.uId!),
             Padding(
               padding: EdgeInsets.only(
                 right: 8.w,
---
+++ lib/src/features/chat/presentation/widgets/messages_stream_builder.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:icare/src/core/helpers/helper.dart';
+import 'package:icare/src/core/widgets/custom_circular_progress_indicator.dart';
+import 'package:icare/src/features/chat/data/models/message_model.dart';
+import 'package:icare/src/features/chat/presentation/cubit/chat_cubit.dart';
+import 'package:icare/src/features/chat/presentation/widgets/custom_message_bubble.dart';
+
+class MessagesStreamBuilder extends StatelessWidget {
+  const MessagesStreamBuilder({
+    super.key,
+    required this.receiverId,
+  });
+
+  final String receiverId;
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+      stream: context.read<ChatCubit>().messagesStream(receiverId),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CustomCircularProgressIndicator(),
+          );
+        }
+        final messages = snapshot.data?.docs
+            .map((doc) => MessageModel.fromJson(doc.data()))
+            .toList();
+        return Expanded(
+          child: messages!.isNotEmpty
+              ? ListView.builder(
+                  padding: EdgeInsets.zero,
+                  reverse: true,
+                  itemCount: messages.length,
+                  itemBuilder: (_, index) {
+                    return CustomMessageBubble(
+                      message: messages[index],
+                      isMe: Helper.uId == messages[index].senderId,
+                    );
+                  },
+                )
+              : const SizedBox.shrink(),
+        );
+      },
+    );
+  }
+}
