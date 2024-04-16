2756f810832d0b2e83acb0da39f2d5ed99d95888
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-16 13:25:06 +0200

    Update LevelsList widget

--- lib/src/features/speech_therapy/presentation/widgets/levels_list.dart
+++ lib/src/features/speech_therapy/presentation/widgets/levels_list.dart
@@ -1,6 +1,11 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:icare/src/config/router/app_router.dart';
 import 'package:icare/src/core/utils/app_constants.dart';
 import 'package:icare/src/core/utils/app_strings.dart';
+import 'package:icare/src/features/speech_therapy/presentation/cubit/speech_therapy_cubit.dart';
 import 'package:icare/src/features/speech_therapy/presentation/widgets/speech_therapy_level_item.dart';
 
 class LevelsList extends StatelessWidget {
@@ -11,18 +16,22 @@ class LevelsList extends StatelessWidget {
     return Column(
       children: List.generate(
         AppConstants.speechTherapyLevelsImages.length,
-        (index) => SpeechTherapyLevelItem(
-          onTap: () => moveToLevelDetails(index),
-          image: AppConstants.speechTherapyLevelsImages[index],
-          title: AppConstants.speechTherapyLevelsTitles[index],
-          description: AppConstants.speechTherapyLevelsDescriptions[index],
+        (index) => JelloIn(
+          child: SpeechTherapyLevelItem(
+            onTap: () => moveToLevelDetails(index, context),
+            image: AppConstants.speechTherapyLevelsImages[index],
+            title: AppConstants.speechTherapyLevelsTitles[index],
+            description: AppConstants.speechTherapyLevelsDescriptions[index],
+          ),
         ),
       ),
     );
   }
 
-  void moveToLevelDetails(int index) {
+  void moveToLevelDetails(int index, BuildContext context) {
     if (AppConstants.speechTherapyLevelsTitles[index] == AppStrings.levelOne) {
+      context.read<SpeechTherapyCubit>().getLevelOneTrainingData();
+      context.pushRoute(const LevelOneRoute());
     } else if (AppConstants.speechTherapyLevelsTitles[index] ==
         AppStrings.levelTwo) {
     } else if (AppConstants.speechTherapyLevelsTitles[index] ==
