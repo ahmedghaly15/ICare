81ab277088d58d6715fdae252ee491aa1812bc64
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-16 08:32:14 +0200

    Build AnimatedEmptyFollowingOrFollowers widget  - Update FollowingBlocBuilder, FollowersBlocBuilder widgets

---
+++ lib/src/features/following_and_followers/presentation/widgets/animated_empty_following_or_followers.dart
@@ -0,0 +1,48 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:icare/src/config/themes/app_text_styles.dart';
+import 'package:icare/src/core/utils/app_assets.dart';
+import 'package:icare/src/core/widgets/my_sized_box.dart';
+
+class AnimatedEmptyFollowingOrFollowers extends StatelessWidget {
+  const AnimatedEmptyFollowingOrFollowers({
+    super.key,
+    this.svgImage = AppAssets.svgsEmptyFollowersOrFollowing,
+    required this.title,
+    required this.description,
+  });
+
+  final String svgImage, title, description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FadeInDown(
+            child: SvgPicture.asset(svgImage),
+          ),
+          MySizedBox.height25,
+          FadeInUp(
+            child: Text(
+              title,
+              style: AppTextStyles.textStyle30Bold(context),
+            ),
+          ),
+          MySizedBox.height5,
+          FadeInUp(
+            child: Text(
+              description,
+              style: AppTextStyles.textStyle16Regular(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
