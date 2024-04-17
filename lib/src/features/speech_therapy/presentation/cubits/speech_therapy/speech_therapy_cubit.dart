797e2d61a291d9632bce8044a0b8f7ce01992a81
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-17 07:52:19 +0200

    Un Depend SpeechTherapyCubit on MarkUseCase

--- lib/src/features/speech_therapy/presentation/cubits/speech_therapy/speech_therapy_cubit.dart
+++ lib/src/features/speech_therapy/presentation/cubits/speech_therapy/speech_therapy_cubit.dart
@@ -1,41 +1,24 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:icare/src/core/helpers/helper.dart';
-import 'package:icare/src/features/speech_therapy/data/models/mark_params.dart';
 import 'package:icare/src/features/speech_therapy/domain/usecases/get_level_one_training_data.dart';
-import 'package:icare/src/features/speech_therapy/domain/usecases/mark_level_one.dart';
 import 'package:icare/src/features/speech_therapy/presentation/cubits/speech_therapy/speech_therapy_state.dart';
 
 class SpeechTherapyCubit extends Cubit<SpeechTherapyState> {
-  final GetLevelOneTrainingDataUseCase getLevelOneTrainingDataUseCase;
-  final MarkLevelOneUseCase markLevelOneUseCase;
+  final GetLevelOneTrainingDataUseCase _getLevelOneTrainingDataUseCase;
 
-  SpeechTherapyCubit({
-    required this.getLevelOneTrainingDataUseCase,
-    required this.markLevelOneUseCase,
-  }) : super(const SpeechTherapyState.initial());
+  SpeechTherapyCubit(
+    this._getLevelOneTrainingDataUseCase,
+  ) : super(const SpeechTherapyState.initial());
 
   void getLevelOneTrainingData() async {
     emit(const SpeechTherapyState.getLevelOneTrainingDataLoading());
-    final result = await getLevelOneTrainingDataUseCase.call(Helper.uId!);
+    final result = await _getLevelOneTrainingDataUseCase.call(Helper.uId!);
     result.when(
       success: (data) =>
           emit(SpeechTherapyState.getLevelOneTrainingDataSuccess(data)),
       error: (error) => emit(
         SpeechTherapyState.getLevelOneTrainingDataError(
             error.apiErrorModel.error ?? ''),
-      ),
-    );
-  }
-
-  void markLevelOneExam(MarkParams params) async {
-    emit(const SpeechTherapyState.markLevelOneExamLoading());
-    final result = await markLevelOneUseCase.call(params);
-    result.when(
-      success: (data) => emit(SpeechTherapyState.markLevelOneExamSuccess(data)),
-      error: (error) => emit(
-        SpeechTherapyState.markLevelOneExamError(
-          error.apiErrorModel.error ?? '',
-        ),
       ),
     );
   }
