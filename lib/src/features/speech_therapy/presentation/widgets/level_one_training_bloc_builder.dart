76b1a065880d6e06a3dbc3f26ee7d0092a4f3ecf
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-17 13:20:53 +0200

    Update LevelOneTrainingBlocBuilder widget

--- lib/src/features/speech_therapy/presentation/widgets/level_one_training_bloc_builder.dart
+++ lib/src/features/speech_therapy/presentation/widgets/level_one_training_bloc_builder.dart
@@ -1,9 +1,13 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:icare/src/config/themes/app_colors.dart';
 import 'package:icare/src/core/utils/app_constants.dart';
+import 'package:icare/src/core/utils/app_strings.dart';
 import 'package:icare/src/core/widgets/custom_error_widget.dart';
 import 'package:icare/src/core/widgets/my_sized_box.dart';
+import 'package:icare/src/core/widgets/primary_button.dart';
 import 'package:icare/src/core/widgets/shimmer_widget.dart';
 import 'package:icare/src/features/speech_therapy/presentation/cubits/speech_therapy/speech_therapy_cubit.dart';
 import 'package:icare/src/features/speech_therapy/presentation/cubits/speech_therapy/speech_therapy_state.dart';
@@ -22,31 +26,53 @@ class LevelOneTrainingBlocBuilder extends StatelessWidget {
           current is GetLevelOneTrainingDataError,
       builder: (context, state) {
         if (state is GetLevelOneTrainingDataSuccess) {
-          return ListView.separated(
-            padding: EdgeInsets.zero,
-            itemBuilder: (context, index) => state.data[index].details.available
-                ? (index % 2 == 0
-                    ? FadeInDown(
-                        from: 50,
-                        child: LevelOneTrainingItem(data: state.data[index]),
-                      )
-                    : FadeInUp(
-                        from: 50,
-                        child: LevelOneTrainingItem(data: state.data[index]),
-                      ))
-                : (index % 2 == 0
-                    ? FadeInDown(
-                        from: 50,
-                        child: BlurredLevelOneTrainingItem(
-                            data: state.data[index]),
-                      )
-                    : FadeInUp(
-                        from: 50,
-                        child: BlurredLevelOneTrainingItem(
-                            data: state.data[index]),
-                      )),
-            separatorBuilder: (_, __) => MySizedBox.height15,
-            itemCount: state.data.length,
+          return Column(
+            children: <Widget>[
+              Expanded(
+                child: ListView.separated(
+                  padding: EdgeInsets.zero,
+                  itemBuilder: (context, index) => state
+                          .data[index].details.available
+                      ? (index % 2 == 0
+                          ? FadeInDown(
+                              from: 50,
+                              child:
+                                  LevelOneTrainingItem(data: state.data[index]),
+                            )
+                          : FadeInUp(
+                              from: 50,
+                              child:
+                                  LevelOneTrainingItem(data: state.data[index]),
+                            ))
+                      : (index % 2 == 0
+                          ? FadeInDown(
+                              from: 50,
+                              child: BlurredLevelOneTrainingItem(
+                                  data: state.data[index]),
+                            )
+                          : FadeInUp(
+                              from: 50,
+                              child: BlurredLevelOneTrainingItem(
+                                  data: state.data[index]),
+                            )),
+                  separatorBuilder: (_, __) => MySizedBox.height15,
+                  itemCount: state.data.length,
+                ),
+              ),
+              MySizedBox.height27,
+              Container(
+                padding: EdgeInsets.all(16.h),
+                decoration: BoxDecoration(
+                  color: AppColors.secondaryColor,
+                  borderRadius: BorderRadius.circular(25.r),
+                ),
+                child: PrimaryButton(
+                  text: AppStrings.score,
+                  hasShadow: false,
+                  onPressed: () {},
+                ),
+              ),
+            ],
           );
         } else if (state is GetLevelOneTrainingDataError) {
           return CustomErrorWidget(
