08f5c831138631d587c76d22714c0d4080bb484d
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-18 09:47:04 +0200

    Fix Null Check Bug in NewTinyTaleView

--- lib/src/features/tiny_tales/presentation/widgets/current_user_name_and_image.dart
+++ lib/src/features/tiny_tales/presentation/widgets/current_user_name_and_image.dart
@@ -1,31 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:icare/src/config/themes/app_colors.dart';
+import 'package:icare/src/config/themes/app_text_styles.dart';
 import 'package:icare/src/core/helpers/helper.dart';
 import 'package:icare/src/core/widgets/custom_cached_network_image.dart';
+import 'package:icare/src/core/widgets/loading_user_item.dart';
 import 'package:icare/src/core/widgets/my_sized_box.dart';
-import 'package:icare/src/features/profile/presentation/widgets/current_user_name_bloc_builder.dart';
+import 'package:icare/src/features/user/presentation/cubit/user_cubit.dart';
+import 'package:icare/src/features/user/presentation/cubit/user_state.dart';
 
 class CurrentUsernameAndImage extends StatelessWidget {
   const CurrentUsernameAndImage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        CustomCachedNetworkImage(
-          imageUrl: Helper.currentUser!.profileImage!,
-          imageBuilder: (_, image) {
-            return CircleAvatar(
-              backgroundImage: image,
-              radius: 25.r,
-              backgroundColor: AppColors.primaryColor,
-            );
-          },
-        ),
-        MySizedBox.width10,
-        const Expanded(child: CurrentUserNameBlocBuilder()),
-      ],
+    return BlocBuilder<UserCubit, UserState>(
+      buildWhen: (_, current) => current is GetUserDataSuccess,
+      builder: (context, state) {
+        if (state is GetUserDataSuccess) {
+          return Row(
+            children: <Widget>[
+              CustomCachedNetworkImage(
+                imageUrl: Helper.currentUser!.profileImage!,
+                imageBuilder: (_, image) {
+                  return CircleAvatar(
+                    backgroundImage: image,
+                    radius: 25.r,
+                    backgroundColor: AppColors.primaryColor,
+                  );
+                },
+              ),
+              MySizedBox.width10,
+              Expanded(
+                child: Text(
+                  Helper.currentUser!.name!,
+                  style: AppTextStyles.textStyle25Bold(context),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          );
+        } else {
+          return const LoadingUserItem();
+        }
+      },
     );
   }
 }
--- lib/src/features/tiny_tales/presentation/widgets/new_tiny_tale_text_field.dart
+++ lib/src/features/tiny_tales/presentation/widgets/new_tiny_tale_text_field.dart
@@ -1,33 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:icare/src/config/themes/app_text_styles.dart';
 import 'package:icare/src/core/helpers/helper.dart';
 import 'package:icare/src/core/widgets/custom_text_form_field.dart';
+import 'package:icare/src/core/widgets/shimmer_widget.dart';
 import 'package:icare/src/features/tiny_tales/presentation/cubits/new_tiny_tale/new_tiny_tale_cubit.dart';
+import 'package:icare/src/features/user/presentation/cubit/user_cubit.dart';
+import 'package:icare/src/features/user/presentation/cubit/user_state.dart';
 
 class NewTinyTaleTextField extends StatelessWidget {
   const NewTinyTaleTextField({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomTextFormField(
-      hintStyle: AppTextStyles.textStyle20Regular(context).copyWith(
-        color: Colors.grey,
-      ),
-      autofocus: true,
-      style: AppTextStyles.textStyle20Regular(context),
-      maxLines: null,
-      controller: context.read<NewTinyTaleCubit>().createNewTinyTaleController,
-      hintText:
-          "What's in your mind, ${Helper.currentUser!.name!.split(' ')[0]}",
-      enableSuggestions: true,
-      contentPadding: EdgeInsets.zero,
-      filled: false,
-      textCapitalization: TextCapitalization.sentences,
-      keyboardType: TextInputType.multiline,
-      border: InputBorder.none,
-      onChanged: (String value) =>
-          context.read<NewTinyTaleCubit>().setNewTinyTaleText(value),
+    return BlocBuilder<UserCubit, UserState>(
+      buildWhen: (_, current) => current is GetUserDataSuccess,
+      builder: (context, state) {
+        if (state is GetUserDataSuccess) {
+          return CustomTextFormField(
+            hintStyle: AppTextStyles.textStyle20Regular(context).copyWith(
+              color: Colors.grey,
+            ),
+            autofocus: true,
+            style: AppTextStyles.textStyle20Regular(context),
+            maxLines: null,
+            controller:
+                context.read<NewTinyTaleCubit>().createNewTinyTaleController,
+            hintText:
+                "What's in your mind, ${Helper.currentUser!.name!.split(' ')[0]}",
+            enableSuggestions: true,
+            contentPadding: EdgeInsets.zero,
+            filled: false,
+            textCapitalization: TextCapitalization.sentences,
+            keyboardType: TextInputType.multiline,
+            border: InputBorder.none,
+            onChanged: (String value) =>
+                context.read<NewTinyTaleCubit>().setNewTinyTaleText(value),
+          );
+        } else {
+          return ShimmerWidget(
+            height: 16.h,
+            width: double.infinity,
+          );
+        }
+      },
     );
   }
 }
