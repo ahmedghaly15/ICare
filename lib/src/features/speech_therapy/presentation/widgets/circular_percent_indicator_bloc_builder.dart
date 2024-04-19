b3453d9bc4b79f086fa229efdb2f1e1048cbf6e4
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-19 07:34:06 +0200

    Build CustomCircularPercentIndicator widget  - Update CircularPercentIndicatorBlocBuilder widget

--- lib/src/features/speech_therapy/presentation/widgets/circular_percent_indicator_bloc_builder.dart
+++ lib/src/features/speech_therapy/presentation/widgets/circular_percent_indicator_bloc_builder.dart
@@ -1,13 +1,10 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:icare/src/config/themes/app_colors.dart';
-import 'package:icare/src/config/themes/app_text_styles.dart';
 import 'package:icare/src/core/widgets/my_sized_box.dart';
 import 'package:icare/src/features/speech_therapy/presentation/cubits/level_training/level_training_cubit.dart';
 import 'package:icare/src/features/speech_therapy/presentation/cubits/level_training/level_training_state.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:icare/src/features/speech_therapy/presentation/widgets/custom_circular_percent_indicator.dart';
 
 class CircularPercentIndicatorBlocBuilder extends StatelessWidget {
   const CircularPercentIndicatorBlocBuilder({super.key});
@@ -23,21 +20,8 @@ class CircularPercentIndicatorBlocBuilder extends StatelessWidget {
             children: <Widget>[
               FadeInDown(
                 from: 50,
-                child: CircularPercentIndicator(
-                  radius: 50.r,
-                  percent: state.data.percent / 100,
-                  progressColor: AppColors.primaryColor,
-                  backgroundColor: Colors.transparent,
-                  animation: true,
-                  animationDuration: 800,
-                  fillColor: Colors.transparent,
-                  center: Text(
-                    '${state.data.percent}%',
-                    style: AppTextStyles.textStyle20Bold(context).copyWith(
-                      color: AppColors.primaryColor,
-                    ),
-                  ),
-                ),
+                child:
+                    CustomCircularPercentIndicator(percent: state.data.percent),
               ),
               MySizedBox.height25,
             ],
---
+++ lib/src/features/speech_therapy/presentation/widgets/custom_circular_percent_indicator.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:icare/src/config/themes/app_colors.dart';
+import 'package:icare/src/config/themes/app_text_styles.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+
+class CustomCircularPercentIndicator extends StatelessWidget {
+  const CustomCircularPercentIndicator({
+    super.key,
+    required this.percent,
+  });
+
+  final double percent;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircularPercentIndicator(
+      radius: 50.r,
+      percent: percent / 100,
+      progressColor: AppColors.primaryColor,
+      backgroundColor: Colors.transparent,
+      animation: true,
+      animationDuration: 800,
+      fillColor: Colors.transparent,
+      center: Text(
+        '$percent %',
+        style: AppTextStyles.textStyle20Bold(context).copyWith(
+          color: AppColors.primaryColor,
+        ),
+      ),
+    );
+  }
+}
