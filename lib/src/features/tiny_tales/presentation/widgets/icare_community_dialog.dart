0c9c8fc4d6abfb7f80818f73bce6dcf2dd929aa2
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-23 09:09:17 +0200

    Refactor ICareCommunityDialog

--- lib/src/features/tiny_tales/presentation/widgets/icare_community_dialog.dart
+++ lib/src/features/tiny_tales/presentation/widgets/icare_community_dialog.dart
@@ -3,56 +3,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:icare/src/config/router/app_router.dart';
 import 'package:icare/src/config/themes/app_text_styles.dart';
-import 'package:icare/src/core/utils/app_constants.dart';
 import 'package:icare/src/core/utils/app_strings.dart';
-import 'package:icare/src/core/widgets/custom_animated_dialog.dart';
 import 'package:icare/src/core/widgets/my_sized_box.dart';
 import 'package:icare/src/features/tiny_tales/presentation/cubits/tiny_tales/tiny_tales_cubit.dart';
 
-class ICareCommunityDialog {
-  static Future show(BuildContext context) async {
-    return await showGeneralDialog(
-      context: context,
-      barrierDismissible: true,
-      barrierLabel: '',
-      pageBuilder: (context, _, __) => const SizedBox.shrink(),
-      transitionDuration: AppConstants.dialogsTransitionDuration,
-      transitionBuilder: (context, animation1, animation2, widget) {
-        return CustomAnimatedDialog(
-          animation1: animation1,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              MySizedBox.height8,
-              ListTile(
-                title: TextButton.icon(
-                  onPressed: () {
-                    context.router.popAndPush(const SearchUsersRoute());
-                  },
-                  icon: const Icon(Icons.search),
-                  label: const Text(AppStrings.search),
-                  style: TextButton.styleFrom(
-                    textStyle: AppTextStyles.textStyle18Bold,
-                  ),
-                ),
-              ),
-              ListTile(
-                title: TextButton.icon(
-                  onPressed: () {
-                    context.router.popAndPush(const BookmarkedTinyTalesRoute());
-                    context.read<TinyTalesCubit>().getBookmarkedTinyTales();
-                  },
-                  icon: const Icon(Icons.bookmark),
-                  label: const Text(AppStrings.bookmarks),
-                  style: TextButton.styleFrom(
-                    textStyle: AppTextStyles.textStyle18Bold,
-                  ),
-                ),
-              ),
-            ],
+class ICareCommunityDialog extends StatelessWidget {
+  const ICareCommunityDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        MySizedBox.height8,
+        ListTile(
+          title: TextButton.icon(
+            onPressed: () {
+              context.router.popAndPush(const SearchUsersRoute());
+            },
+            icon: const Icon(Icons.search),
+            label: const Text(AppStrings.search),
+            style: TextButton.styleFrom(
+              textStyle: AppTextStyles.textStyle18Bold,
+            ),
           ),
-        );
-      },
+        ),
+        ListTile(
+          title: TextButton.icon(
+            onPressed: () {
+              context.router.popAndPush(const BookmarkedTinyTalesRoute());
+              context.read<TinyTalesCubit>().getBookmarkedTinyTales();
+            },
+            icon: const Icon(Icons.bookmark),
+            label: const Text(AppStrings.bookmarks),
+            style: TextButton.styleFrom(
+              textStyle: AppTextStyles.textStyle18Bold,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/src/features/tiny_tales/presentation/widgets/icare_community_sliver_app_bar.dart
+++ lib/src/features/tiny_tales/presentation/widgets/icare_community_sliver_app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:icare/src/core/utils/app_assets.dart';
 import 'package:icare/src/core/utils/app_strings.dart';
 import 'package:icare/src/core/widgets/custom_sliver_app_bar.dart';
+import 'package:icare/src/core/widgets/icare_dialog.dart';
 import 'package:icare/src/features/tiny_tales/presentation/widgets/icare_community_dialog.dart';
 
 class ICareCommunitySliverAppBar extends StatelessWidget {
@@ -18,7 +19,10 @@ class ICareCommunitySliverAppBar extends StatelessWidget {
       actions: <Widget>[
         IconButton(
           onPressed: () {
-            ICareCommunityDialog.show(context);
+            ShowICareDialog.show(
+              context: context,
+              child: const ICareCommunityDialog(),
+            );
           },
           icon: Image.asset(AppAssets.imagesAppLogo),
         ),
