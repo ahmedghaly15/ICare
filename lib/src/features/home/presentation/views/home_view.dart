d709600ec965e74a6181cca0d91a123a4e36702a
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-04-23 09:11:38 +0200

    Build HomeViewBody widget & Refactor HomeView

--- lib/src/features/home/presentation/views/home_view.dart
+++ lib/src/features/home/presentation/views/home_view.dart
@@ -1,16 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:icare/dependency_injection.dart';
-import 'package:icare/src/config/themes/app_colors.dart';
-import 'package:icare/src/config/themes/app_text_styles.dart';
-import 'package:icare/src/core/utils/app_strings.dart';
-import 'package:icare/src/core/utils/size_config.dart';
-import 'package:icare/src/features/home/presentation/widgets/ai_features_sliver_list.dart';
 import 'package:icare/src/features/home/presentation/widgets/custom_drawer.dart';
+import 'package:icare/src/features/home/presentation/widgets/home_view_body.dart';
 import 'package:icare/src/features/tips/presentation/cubit/tips_cubit.dart';
-import 'package:icare/src/features/tips/presentation/widgets/random_tip_dialog.dart';
 
 @RoutePage()
 class HomeView extends StatelessWidget implements AutoRouteWrapper {
@@ -26,42 +20,9 @@ class HomeView extends StatelessWidget implements AutoRouteWrapper {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            const SliverAppBar(),
-            SliverToBoxAdapter(
-              child: Container(
-                margin: EdgeInsets.only(
-                  top: SizeConfig.height * 0.05,
-                  bottom: 16.h,
-                ),
-                child: const RandomTipDialog(),
-              ),
-            ),
-            SliverPadding(
-              padding: EdgeInsets.symmetric(horizontal: 16.w),
-              sliver: SliverToBoxAdapter(
-                child: Text(
-                  AppStrings.aiAssistedChildCare,
-                  style: AppTextStyles.textStyle20Bold.copyWith(
-                    color: AppColors.primaryColor,
-                  ),
-                ),
-              ),
-            ),
-            SliverPadding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 16.w,
-                vertical: 10.h,
-              ),
-              sliver: const AIFeaturesSliverList(),
-            ),
-          ],
-        ),
-      ),
-      drawer: const CustomDrawer(),
+    return const Scaffold(
+      body: HomeViewBody(),
+      drawer: CustomDrawer(),
     );
   }
 }
---
+++ lib/src/features/home/presentation/widgets/home_view_body.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:icare/src/config/themes/app_colors.dart';
+import 'package:icare/src/config/themes/app_text_styles.dart';
+import 'package:icare/src/core/utils/app_assets.dart';
+import 'package:icare/src/core/utils/app_strings.dart';
+import 'package:icare/src/core/utils/size_config.dart';
+import 'package:icare/src/core/widgets/icare_dialog.dart';
+import 'package:icare/src/features/home/presentation/widgets/ai_features_sliver_list.dart';
+import 'package:icare/src/features/tips/presentation/cubit/tips_cubit.dart';
+import 'package:icare/src/features/tips/presentation/cubit/tips_state.dart';
+import 'package:icare/src/features/tips/presentation/widgets/random_tip_dialog.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            actions: <Widget>[
+              BlocBuilder<TipsCubit, TipsState>(
+                builder: (context, state) {
+                  if (context.read<TipsCubit>().isDone) {
+                    return IconButton(
+                      icon: Image.asset(AppAssets.imagesAppLogo),
+                      onPressed: () {
+                        ShowICareDialog.show(
+                          context: context,
+                          message: context.read<TipsCubit>().randomTip!.info,
+                          anotherTitle: AppStrings.dailyRandomTip,
+                        );
+                      },
+                    );
+                  } else {
+                    return const SizedBox.shrink();
+                  }
+                },
+              ),
+            ],
+          ),
+          BlocBuilder<TipsCubit, TipsState>(
+            builder: (context, state) {
+              return context.read<TipsCubit>().isDone
+                  ? const SliverToBoxAdapter(child: SizedBox.shrink())
+                  : SliverToBoxAdapter(
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          top: SizeConfig.height * 0.05,
+                          bottom: 16.h,
+                        ),
+                        child: const RandomTipDialog(),
+                      ),
+                    );
+            },
+          ),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            sliver: SliverToBoxAdapter(
+              child: Text(
+                AppStrings.aiAssistedChildCare,
+                style: AppTextStyles.textStyle20Bold.copyWith(
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.w,
+              vertical: 10.h,
+            ),
+            sliver: const AIFeaturesSliverList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
