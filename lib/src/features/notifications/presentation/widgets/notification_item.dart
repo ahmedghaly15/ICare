f1e9f5a624f775021c5c1d0fc57952566a095666
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-05-01 14:43:09 +0300

    Updated SliverNotificationsStreamBuilder & Refactored NotificationItem

--- lib/src/features/notifications/presentation/widgets/notification_item.dart
+++ lib/src/features/notifications/presentation/widgets/notification_item.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:icare/src/config/themes/app_text_styles.dart';
 import 'package:icare/src/core/utils/functions/is_dark_mode_active.dart';
+import 'package:icare/src/core/utils/functions/navigate_to_user_profile.dart';
 import 'package:icare/src/core/widgets/custom_cached_network_image.dart';
+import 'package:icare/src/core/widgets/my_sized_box.dart';
 import 'package:icare/src/features/notifications/data/models/icare_notification.dart';
+import 'package:icare/src/features/notifications/presentation/cubits/notifications_cubit.dart';
 
 class NotificationItem extends StatelessWidget {
   const NotificationItem({
@@ -15,38 +19,72 @@ class NotificationItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {},
-      leading: CustomCachedNetworkImage(
-        height: 32.h,
-        width: 32.h,
-        imageUrl: notification.user!.profileImage!,
-        imageBuilder: (_, image) => CircleAvatar(
-          radius: 16.r,
-          backgroundImage: image,
-        ),
-      ),
-      title: notification.isMessage!
-          ? Text(
-              notification.title,
-              style: AppTextStyles.textStyle13Bold.copyWith(
-                color: isDarkModeActive(context) ? Colors.white : Colors.black,
-              ),
-            )
-          : Text(
-              notification.body,
-              style: AppTextStyles.textStyle13Bold.copyWith(
-                color: isDarkModeActive(context) ? Colors.white : Colors.black,
+    return MaterialButton(
+      padding: EdgeInsets.symmetric(vertical: 12.h),
+      onPressed: () {
+        context.read<NotificationsCubit>().readNotification(notification.id!);
+        context
+            .read<NotificationsCubit>()
+            .navigateToNotificationView(context, notification);
+      },
+      child: Row(
+        children: <Widget>[
+          InkWell(
+            onTap: () {
+              navigateToUserProfile(context: context, user: notification.user!);
+            },
+            child: CustomCachedNetworkImage(
+              imageUrl: notification.user!.profileImage!,
+              imageBuilder: (_, image) => CircleAvatar(
+                radius: 16.r,
+                backgroundImage: image,
               ),
             ),
-      subtitle: notification.isMessage!
-          ? Text(
-              notification.body,
-              style: AppTextStyles.textStyle10Regular.copyWith(
-                color: isDarkModeActive(context) ? Colors.white : Colors.black,
-              ),
-            )
-          : null,
+          ),
+          MySizedBox.width10,
+          notification.isMessage!
+              ? Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        notification.title,
+                        style: AppTextStyles.textStyle13Bold.copyWith(
+                          color: isDarkModeActive(context)
+                              ? Colors.white
+                              : Colors.black,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      MySizedBox.height5,
+                      Text(
+                        notification.body,
+                        style: AppTextStyles.textStyle10Regular.copyWith(
+                          color: isDarkModeActive(context)
+                              ? Colors.white
+                              : Colors.black,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                )
+              : Expanded(
+                  child: Text(
+                    notification.body,
+                    style: AppTextStyles.textStyle13Bold.copyWith(
+                      color: isDarkModeActive(context)
+                          ? Colors.white
+                          : Colors.black,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+        ],
+      ),
     );
   }
 }
--- lib/src/features/notifications/presentation/widgets/sliver_notifications_stream_builder.dart
+++ lib/src/features/notifications/presentation/widgets/sliver_notifications_stream_builder.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:icare/src/core/widgets/custom_circular_progress_indicator.dart';
 import 'package:icare/src/features/notifications/data/models/icare_notification.dart';
 import 'package:icare/src/features/notifications/presentation/cubits/notifications_cubit.dart';
@@ -27,11 +28,14 @@ class SliverNotificationsStreamBuilder extends StatelessWidget {
                 ICareNotification.fromJson(notification.data()))
             .toList();
         return notifications.isNotEmpty
-            ? SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) =>
-                      NotificationItem(notification: notifications[index]),
-                  childCount: notifications.length,
+            ? SliverPadding(
+                padding: EdgeInsets.symmetric(horizontal: 9.w),
+                sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) =>
+                        NotificationItem(notification: notifications[index]),
+                    childCount: notifications.length,
+                  ),
                 ),
               )
             : const SliverFillRemaining(child: SizedBox.shrink());
