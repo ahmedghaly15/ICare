2a7835a9dae9ba3d2e9e7fdccc7d3042d33b7696
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-05-24 10:18:31 +0300

    Updated EntryView & main_prod

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-import 'package:icare/bloc_observer.dart';
 import 'package:icare/dependency_injection.dart';
 import 'package:icare/src/core/firebase/firebase_options.dart';
+import 'package:icare/src/core/services/local_notifications/local_notifications_service.dart';
 import 'package:icare/src/core/utils/app_strings.dart';
 import 'package:icare/src/icare_app.dart';
 
@@ -16,6 +15,6 @@ void main() async {
   );
   await dotenv.load(fileName: AppStrings.dotEnvFileName);
   await DependencyInjection().setup();
-  Bloc.observer = MyBlocObserver();
+  await getIt.get<LocalNotificationsService>().initLocalNotifications();
   runApp(const ICareApp());
 }
--- lib/src/features/entry/presentation/views/entry_view.dart
+++ lib/src/features/entry/presentation/views/entry_view.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,9 +6,8 @@ import 'package:icare/dependency_injection.dart';
 import 'package:icare/src/config/router/app_router.dart';
 import 'package:icare/src/core/helpers/cache_helper.dart';
 import 'package:icare/src/core/helpers/helper.dart';
-import 'package:icare/src/core/services/local_notifications/local_notification.dart';
-import 'package:icare/src/core/services/local_notifications/local_notifications_service.dart';
 import 'package:icare/src/core/utils/app_strings.dart';
+import 'package:icare/src/core/utils/functions/is_dark_mode_active.dart';
 import 'package:icare/src/features/user/presentation/cubit/user_cubit.dart';
 
 @RoutePage()
@@ -46,44 +44,29 @@ class _EntryViewState extends State<EntryView> {
   void initState() {
     super.initState();
     // To ensure that navigation calls are performed after the widget tree has been built
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _goToNextView();
-    });
-    _handleForegroundNotification();
+    WidgetsBinding.instance.addPostFrameCallback((_) => _goToNextView());
   }
 
-  void _handleForegroundNotification() {
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      debugPrint(
-        '********* FOREGROUND NOTIFICATION *********\n NOTIFICATION TITLE: ${message.notification!.title}\n NOTIFICATION BODY: ${message.notification!.body}',
-      );
-      if (message.notification != null) {
-        getIt.get<LocalNotificationsService>().showLocalNotification(
-              LocalNotification(
-                title: message.notification!.title!,
-                body: message.notification!.body!,
-              ),
-            );
-      }
-    });
-  }
-
-  void _setSystemUIOverlayStyle() {
+  void _setSystemUIOverlayStyle(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 
     SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
+      SystemUiOverlayStyle(
         // systemNavigationBarColor: Colors.transparent,
         statusBarColor: Colors.transparent,
-        statusBarBrightness: Brightness.dark,
-        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness:
+            isDarkModeActive(context) ? Brightness.dark : Brightness.light,
+        statusBarIconBrightness:
+            isDarkModeActive(context) ? Brightness.light : Brightness.dark,
+        systemNavigationBarIconBrightness:
+            isDarkModeActive(context) ? Brightness.dark : Brightness.light,
       ),
     );
   }
 
   @override
   void didChangeDependencies() {
-    _setSystemUIOverlayStyle();
+    _setSystemUIOverlayStyle(context);
     super.didChangeDependencies();
   }
 
