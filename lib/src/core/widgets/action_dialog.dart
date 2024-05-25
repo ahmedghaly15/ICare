ff333f4f72e0d8408ce29971da864892dd19304a
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-05-25 12:37:27 +0300

    Built ActionDialog & Deleted LogOutDialog

--- lib/src/core/widgets/action_dialog.dart
+++ lib/src/core/widgets/action_dialog.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:icare/src/config/themes/app_text_styles.dart';
-import 'package:icare/src/core/utils/app_strings.dart';
 import 'package:icare/src/core/utils/functions/is_dark_mode_active.dart';
 import 'package:icare/src/core/widgets/my_sized_box.dart';
 import 'package:icare/src/core/widgets/outlined_cancel_button.dart';
-import 'package:icare/src/features/home/presentation/widgets/log_out_button_bloc_listener.dart';
 
-class LogoutDialog extends StatelessWidget {
-  const LogoutDialog({super.key});
+class ActionDialog extends StatelessWidget {
+  const ActionDialog({
+    super.key,
+    required this.text,
+    required this.actionButtonWidget,
+  });
+
+  final String text;
+  final Widget actionButtonWidget;
 
   @override
   Widget build(BuildContext context) {
@@ -15,20 +20,20 @@ class LogoutDialog extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
         Text(
-          AppStrings.sureToLogoutQuestion,
+          text,
           style: AppTextStyles.textStyle15Bold.copyWith(
             color: isDarkModeActive(context) ? Colors.white : Colors.black,
           ),
           textAlign: TextAlign.center,
         ),
         MySizedBox.height20,
-        const Row(
+        Row(
           children: <Widget>[
-            Expanded(
+            const Expanded(
               child: OutlinedCancelButton(),
             ),
             MySizedBox.width15,
-            Expanded(child: LogoutButtonBlocListener()),
+            Expanded(child: actionButtonWidget),
           ],
         ),
       ],
