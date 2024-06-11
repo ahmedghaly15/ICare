bc6013a14b1c8f407ca09f50d49a246a734e73d6
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-06-11 17:34:04 +0300

    Documented navigateToUserProfile & openUrl methods

--- lib/src/core/utils/functions/navigate_to_user_profile.dart
+++ lib/src/core/utils/functions/navigate_to_user_profile.dart
@@ -4,6 +4,14 @@ import 'package:icare/src/config/router/app_router.dart';
 import 'package:icare/src/core/helpers/constants.dart';
 import 'package:icare/src/core/models/icare_user.dart';
 
+/// Navigates to the user profile screen or the current user's profile.
+///
+/// This method checks if the provided [user] ID is different from the current user ID.
+/// If different, it navigates to the specified user's profile screen.
+/// If the same, it navigates to the current user's profile screen if not already there.
+///
+/// [context] is the BuildContext used for navigation.
+/// [user] is the ICareUser object representing the user whose profile should be displayed.
 void navigateToUserProfile({
   required BuildContext context,
   required ICareUser user,
--- lib/src/core/utils/functions/open_url.dart
+++ lib/src/core/utils/functions/open_url.dart
@@ -2,6 +2,19 @@ import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:icare/src/core/helpers/app_regex.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+/// Opens a URL from a [LinkableElement] in an appropriate manner.
+///
+/// This method checks if the link's text is a valid email address using [AppRegex.isEmailValid].
+/// If it is a valid email address, it opens the URL directly.
+/// Otherwise, it opens the URL in an in-app browser view.
+///
+/// [link] is the [LinkableElement] containing the URL to be opened.
+///
+/// Example usage:
+/// ```dart
+/// LinkableElement link = LinkableElement(text: 'example@example.com', url: 'mailto:example@example.com');
+/// openUrl(link);
+/// ```
 void openUrl(LinkableElement link) async {
   if (AppRegex.isEmailValid(link.text)) {
     await launchUrl(Uri.parse(link.url));
